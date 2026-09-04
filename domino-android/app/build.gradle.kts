560f06e9ab22d44993f599fce78cf589bf8dc4e4
adam0012af-ai <adam0012af@gmail.com>
2026-09-04 06:47:53 +0300

    Add Domino app Gradle config

---
+++ domino-android/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.adam.domino"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.adam.domino"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+}
